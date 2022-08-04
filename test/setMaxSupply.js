2419fbf157cd98c971efcae4254431aaff54a0a1
Ivan Lieskov <ivan.lieskov@protonmail.com>
2022-08-04 09:30:38 +0200

    setMaxSupply() function tests

---
+++ test/setMaxSupply.js
@@ -0,0 +1,36 @@
+const { expect } = require("chai")
+const { BigNumber } = require("ethers")
+const { ethers } = require("hardhat")
+
+describe("setMaxSupply tests", function () {
+    beforeEach(async function () {
+        ;[owner, wallet1, wallet2] = await ethers.getSigners()
+
+        family = await ethers.getContractFactory("Family", owner)
+        Family = await family.deploy("50000000000000000", 3, 18)
+
+        await Family.connect(wallet1).mintHuman("Bob", "Alice", "Wellingtone", {
+            value: ethers.utils.parseEther("0.05"),
+        })
+        await Family.connect(wallet1).mintHuman("Charlie", "Ketty", "Soprano", {
+            value: ethers.utils.parseEther("0.05"),
+        })
+    })
+    describe("negative tests", function () {
+        it("should revert if owner trying to set max supply lower than total supply", async function () {
+            await expect(Family.setMaxSupply(0)).to.be.revertedWith("Max supply cannot be lower than total supply")
+        })
+        it("should revert if not the owner trying to set max supply", async function () {
+            await expect(Family.connect(wallet1).setMaxSupply(4)).to.be.revertedWith("Ownable: caller is not the owner")
+        })
+    })
+    describe("positive test", function () {
+        it("should successfully set a new maximum supply", async function () {
+            await Family.setMaxSupply(50)
+            expect(await Family.getMaxSupply()).to.equal(50)
+        })
+        it("should emit an MaxSupplyUpdated event", async () => {
+            await expect(Family.setMaxSupply(100)).to.emit(Family, "MaxSupplyUpdated").withArgs(100)
+        })
+    })
+})
