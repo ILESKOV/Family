bb68b531c9c5c3cf7284e867152aac8f6b178186
Ivan Leskov <ivan.leskov@protonmail.com>
2022-10-18 15:29:53 +0200

    all tests combined in one file

---
+++ test/@index.test.ts
@@ -0,0 +1,9 @@
+describe("Family", function () {
+    require("./constructor.test")
+    require("./mintHuman.test")
+    require("./breeding.test")
+    require("./checkAgeChanging.test")
+    require("./setMaturityAge.test")
+    require("./setMaxSupply.test")
+    require("./SetMintPrice.test")
+})
